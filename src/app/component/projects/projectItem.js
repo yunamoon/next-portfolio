757b3918bbebd70148aaf0620c06a09f475966dd
yunamoon <tpwkao0051@gmail.com>
2024-05-03 00:34:13 +0900

    project complete

--- src/app/component/projects/projectItem.js
+++ src/app/component/projects/projectItem.js
@@ -3,26 +3,79 @@ import Image from "next/image";
 export default function ProjectItem({ projects }) {
   const imgSrc = projects.cover.external.url || projects.cover.file.url;
   const title = projects.properties.Name.title[0].plain_text;
+  const start = projects.properties.WorkPeriod.date.start;
+  const end = projects.properties.WorkPeriod.date.end;
   const description = projects.properties.Description.rich_text[0].plain_text;
+  const tags = projects.properties.Tag.multi_select;
   const github = projects.properties.Github.url;
+  const classify = projects.properties.Classify.rich_text[0].plain_text;
 
   return (
-    <div className="p-6 m-3 bg-slate-400 rounded-md">
+    <>
+      <div className="xl:w-1/4 md:w-1/2 p-4 card">
+        <div className="bg-gray-100 p-6 rounded-lg">
+          <Image
+            className="h-40 rounded w-full object-cover object-center mb-6"
+            alt="cover image"
+            src={imgSrc}
+            quality={0}
+            width={0}
+            height={30}
+            objectFit="cover"
+          />
+          {/* <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content"> */}
+          <h3 className="tracking-widest text-yellow-500 text-xs font-medium title-font">
+            {classify}
+          </h3>
+          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+            {title}
+          </h2>
+          <p className="leading-relaxed text-base">{description}</p>
+
+          <a href={github}>깃허브 바로가기</a>
+
+          <div className="flex items-start mt-2">
+            {tags.map((item) => (
+              <h1
+                className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30 text-xs"
+                key={item.id}
+              >
+                {item.name}
+              </h1>
+            ))}
+          </div>
+        </div>
+      </div>
+      {/* <div className="project-card ">
       <Image
         className="rounded-t-md w-full"
         alt="cover image"
         src={imgSrc}
-        quality={100}
-        width={100}
-        height={100}
-        layout="responsive"
+        quality={0}
+        width={0}
+        height={30}
         objectFit="cover"
       />
       <div className="p-4">
-        <h1>{title}</h1>
-        <h1>{description}</h1>
+        <h1 className="text-2xl">{title}</h1>
+        <h3>{description}</h3>
         <a href={github}> 깃허브 바로가기</a>
+
+        <h3>
+          작업기간 : {start} ~ {end}
+        </h3>
+        <div className="flex items-start mt-2">
+          {tags.map((item) => (
+            <h1
+              className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30"
+              key={item.id}
+            >
+              {item.name}
+            </h1>
+          ))}
+        </div>
       </div>
-    </div>
+    </div> */}
+    </>
   );
 }
--- src/app/project/page.js
+++ src/app/project/page.js
@@ -5,17 +5,50 @@ export default async function Project() {
   const { projects } = await getData();
   console.log("클라이언트" + projects);
 
+  /* // <div classNameName="flex flex-col items-center justify-center min-h-screen px-5 py-10 mb-10">
+    //   <h1 classNameName="text-4xl font-bold sm:text-6xl">
+    //     총 프로젝트 :
+    //     <span classNameName="pl-4 text-blue-500">{projects.results.length}</span>
+    //   </h1>
+    //   <div classNameName="grid md:grid-cols-2 xm:w-full py-10 m-6 gap-8">
+    //     {projects.results.map((item) => (
+    //       <ProjectItem key={item.id} projects={item} />
+    //     ))}
+    //   </div>
+    // </div> */
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen px-5 py-24 mb-10">
-      <h1 className="text-3xl font-bold sm:text-5xl">
-        총 프로젝트 :
-        <span className="pl-4 text-blue-500">{projects.results.length}</span>
-      </h1>
-      <div className="grid md:grid-cols-3 xm:w-full py-10 m-6 gap-8">
-        {projects.results.map((item) => (
-          <ProjectItem key={item.id} projects={item} />
-        ))}
+    <section className="flex min-h-screen flex-col items-center justify-center  text-gray-900 body-font ">
+      <div className="container px-5 py-24 mx-auto ">
+        <div className="flex flex-wrap w-full mb-20 ">
+          <div className="w-full mb-6 lg:mb-0  ">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
+              Projects
+            </h1>
+            <div className="h-1 w-20 bg-yellow-400 rounded"></div>
+            {/* <p className="w-full leading-relaxed text-gray-500 pt-2 -mb-3">
+              실무에서 경험한 프로젝트와 개인적으로 진행한 토이 프로젝트에 대한
+              내용을 기록합니다.
+            </p> */}
+            <p className="w-full leading-relaxed text-gray-500 ">
+              <br className="inline-block" />
+              <span className="text-yellow-400">●</span> Front-End: JavaScript,
+              TypeScript, React.js, Next.js, Dart, Flutter, HTML/CSS
+              <br className="inline-block" />
+              <span className="text-yellow-400">●</span> Back-End: Node.js, Java
+              <br className="inline-block" />{" "}
+              <span className="text-yellow-400">●</span> Database: Firebase,
+              Mongo DB, GCP, AWS
+            </p>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center  flex-wrap -m-4">
+          {projects.results.map((item) => (
+            <ProjectItem key={item.id} projects={item} />
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
