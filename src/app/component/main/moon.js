54654fcb9048d2e129821866745ad1f137712ce9
yunamoon <tpwkao0051@gmail.com>
2024-05-03 01:31:02 +0900

    file name update

--- src/app/component/main/moon.js
+++ src/app/component/main/moon.js
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import Lottie from "react-lottie-player";
-import lottieJson from "../../../../public/Moon";
+import lottieJson from "../../../../public/moon";
 
 export default function Moon() {
   return <Lottie loop animationData={lottieJson} play />;
